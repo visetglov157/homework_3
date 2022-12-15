1cd46976a07d492362c6d5debbd591df7523172e
Vladislav <vladislav33lavrov@gmail.com>
2022-12-15 22:21:05 +0300

    Начало работы exercise1

--- exercise1/Program.cs
+++ exercise1/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.Clear();
+Console.Write("введите число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine("палиндром");
+  }
+  else Console.WriteLine("не палиндром");
+}
+
+if (number!.Length == 5){
+  CheckingNumber(number);
+}
+else Console.WriteLine("это не пятизначное число");
