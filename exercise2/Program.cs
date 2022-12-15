46055a569c556af85863cb6fcb8351dba1195e33
Vladislav <vladislav33lavrov@gmail.com>
2022-12-15 22:29:38 +0300

    exercise2 done

---
+++ exercise2/Program.cs
@@ -0,0 +1,24 @@
+﻿int x1 = Coordinate("x", "M");
+int y1 = Coordinate("y", "M");
+int z1 = Coordinate("z", "M");
+int x2 = Coordinate("x", "N");
+int y2 = Coordinate("y", "N");
+int z2 = Coordinate("z", "N");
+
+int Coordinate(string coorName, string pointName)
+{
+    Console.Write($"Введите координату {coorName} точки {pointName}: ");
+    return Convert.ToInt16(Console.ReadLine());
+}
+
+double Decision(double x1, double x2, 
+                double y1, double y2, 
+                double z1, double z2){
+  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+                   Math.Pow((y2-y1), 2) + 
+                   Math.Pow((z2-z1), 2));
+}
+
+double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+
+Console.WriteLine($"Длина отрезка  {segmentLength}");
